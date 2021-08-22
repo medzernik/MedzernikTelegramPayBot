eb51854f461786594a12ded5f92b6368fd1321c5
medze <medzernik1@gmai.com>
2021-08-22 18:22:07 +0200

    First commit

---
+++ config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"os"
+)
+
+type Config struct {
+	Server struct {
+		Token string `yaml:"token"`
+	}
+}
+
+var Cfg Config
+
+func Initialization() {
+
+	readFile(&Cfg)
+	fmt.Printf("%+v", Cfg)
+}
+
+func processError(err error) {
+	fmt.Println(err)
+	os.Exit(2)
+}
+
+func readFile(cfg *Config) {
+	f, err := os.Open("config/config.yml")
+	if err != nil {
+		processError(err)
+	}
+	defer f.Close()
+
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(cfg)
+	if err != nil {
+		processError(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,91 @@
 package main
 
-import "fmt"
+import (
+	"MedzernikTelegramPayBot/config"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+// Create a struct that mimics the webhook response body
+// https://core.telegram.org/bots/api#update
+type webhookReqBody struct {
+	Message struct {
+		Text string `json:"text"`
+		Chat struct {
+			ID int64 `json:"id"`
+		} `json:"chat"`
+	} `json:"message"`
+}
+
+// Handler This handler is called everytime telegram sends us a webhook event
+func Handler(res http.ResponseWriter, req *http.Request) {
+	// First, decode the JSON response body
+	body := &webhookReqBody{}
+	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
+		fmt.Println("could not decode request body", err)
+		return
+	}
+
+	// Check if the message contains the word "marco"
+	// if not, return without doing anything
+	if !strings.Contains(strings.ToLower(body.Message.Text), "marco") {
+		return
+	}
+
+	// If the text contains marco, call the `sayPolo` function, which
+	// is defined below
+	if err := sayPolo(body.Message.Chat.ID); err != nil {
+		fmt.Println("error in sending reply:", err)
+		return
+	}
+
+	// log a confirmation message if the message is sent successfully
+	fmt.Println("reply sent")
+}
+
+//The below code deals with the process of sending a response message
+// to the user
+
+// Create a struct to conform to the JSON body
+// of the send message request
+// https://core.telegram.org/bots/api#sendmessage
+type sendMessageReqBody struct {
+	ChatID int64  `json:"chat_id"`
+	Text   string `json:"text"`
+}
+
+// sayPolo takes a chatID and sends "polo" to them
+func sayPolo(chatID int64) error {
+	// Create the request body struct
+	reqBody := &sendMessageReqBody{
+		ChatID: chatID,
+		Text:   "Polo!!",
+	}
+	// Create the JSON body from the struct
+	reqBytes, err := json.Marshal(reqBody)
+	if err != nil {
+		return err
+	}
+
+	// Send a post request with your token
+	res, err := http.Post("https://api.telegram.org/bot"+config.Cfg.Server.Token+"/sendMessage", "application/json", bytes.NewBuffer(reqBytes))
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return errors.New("unexpected status" + res.Status)
+	}
+
+	return nil
+}
 
 func main() {
 	fmt.Println("Hello World")
+	go config.Initialization()
+	http.ListenAndServe(":3000", http.HandlerFunc(Handler))
+
 }
